0956a336a7fefa0332bbaa6dd0ef84ef329a0388
pinoChang <z8817176@163.com>
2017-07-03 17:02:04 +0800

    首次提交

---
+++ src/components/Hello.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="main">
+    <h1>hello vue</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'main',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .main{
+    h1{
+      font-size: 500px;
+
+    }
+  }
+
+</style>
